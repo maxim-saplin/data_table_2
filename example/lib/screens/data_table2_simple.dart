8d5dd0e3ee8cd6a6d4c25208c31d1744029afb10
Maxim <smaxmail@gmail.com>
2022-02-03 15:23:14 +0300

    Fixed import warnings from analyzer

--- example/lib/screens/data_table2_simple.dart
+++ example/lib/screens/data_table2_simple.dart
@@ -1,6 +1,5 @@
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 /// Example without datasource
 class DataTable2SimpleDemo extends StatelessWidget {
