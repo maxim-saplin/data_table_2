730ca7df2381367528db69797ace9a081d6cf962
maxim-saplin <smaxmail@gmail.com>
2022-04-29 06:46:02 +0000

    Deploying to gh-pages from @ maxim-saplin/data_table_2@aee23eaedbbdb81b9bf5ed2f08bb8a7202c5f9f3 🚀

--- dev/flutter_service_worker.js.js
+++ dev/flutter_service_worker.js.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "4e1936ab8635d4e38f39e157e11ae3be",
-"/": "4e1936ab8635d4e38f39e157e11ae3be",
-"main.dart.js": "fd37c09fe024814230fc1ca47cc55389",
+"index.html": "00db675afab77a52869a9a7bb3cba789",
+"/": "00db675afab77a52869a9a7bb3cba789",
+"main.dart.js": "36c740f1b1126e8e59dd5547b42a3b5b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
