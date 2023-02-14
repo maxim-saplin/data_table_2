f2b8d3a3af7ab46005a60e5778b4646bbb9b0f79
Maxim Saplin <smaxmail@gmail.com>
2023-02-14 16:35:00 +0300

    Tiding up

--- lib/paginated_data_table_2.dart
+++
@@ -1,9 +0,0 @@
-// This file is a duplicate of data_table_2.dart and aimed
-// to avoid compliation issues when migrating to
-// newer version of the packae with lib exports and /src folder
-// TODO, remove with the next major version (2.4.0)
-@Deprecated('Use "package:data_table_2/data_table_2.dart" instead')
-library data_table_2_depricated;
-
-export 'src/data_table_2.dart';
-export 'src/paginated_data_table_2.dart';
