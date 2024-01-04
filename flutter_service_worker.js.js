9beb82f90ca8bef6700e0f01a5cbc40b69f6d70c
maxim-saplin <maxim-saplin@users.noreply.github.com>
2024-01-04 18:08:55 +0000

    Deploying to gh-pages from @ maxim-saplin/data_table_2@681a717a835ba01ea053a35845e6db33ada3c731 🚀

--- flutter_service_worker.js.js
+++ flutter_service_worker.js.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "b010066b96cdd8e1d7037ebe94676ec4",
-"/": "b010066b96cdd8e1d7037ebe94676ec4",
-"main.dart.js": "21ffe43ec65e238c85fbd8f293e72ed8",
+"index.html": "233a89edc13e5bd20246a4ae2de359c9",
+"/": "233a89edc13e5bd20246a4ae2de359c9",
+"main.dart.js": "98ea14779ed7be74684ddd7f90d35b52",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,11 +22,11 @@ const RESOURCES = {"version.json": "ff966ab969ba381b900e61629bfb9789",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/fonts/MaterialIcons-Regular.otf": "034c6064d4ea041eb5cb9cc8a10bd6e3",
 "canvaskit/skwasm.js": "87063acf45c5e1ab9565dcf06b0c18b8",
-"canvaskit/skwasm.wasm": "4124c42a73efa7eb886d3400a1ed7a06",
+"canvaskit/skwasm.wasm": "2fc47c0a0c3c7af8542b601634fe9674",
 "canvaskit/chromium/canvaskit.js": "0ae8bbcc58155679458a0f7a00f66873",
-"canvaskit/chromium/canvaskit.wasm": "f87e541501c96012c252942b6b75d1ea",
+"canvaskit/chromium/canvaskit.wasm": "143af6ff368f9cd21c863bfa4274c406",
 "canvaskit/canvaskit.js": "eb8797020acdbdf96a12fb0405582c1b",
-"canvaskit/canvaskit.wasm": "64edb91684bdb3b879812ba2e48dd487",
+"canvaskit/canvaskit.wasm": "73584c1a3367e3eaf757647a8f5c5989",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03"};
 // The application shell files that are downloaded before a service worker can
 // start.
