7267d5cfbe33b44022997238040658a9a776a94e
prabakaran <prabakaran.palanisamy@gmail.com>
2024-12-17 12:22:10 +0530

    Updated "withValues" function with "withOpacity" in data_table2.dart. This was causing compile time errors while trying to run

--- lib/src/data_table_2.dart
+++ lib/src/data_table_2.dart
@@ -522,7 +522,7 @@ class DataTable2 extends DataTable {
       child: DefaultTextStyle(
         style: effectiveDataTextStyle.copyWith(
           color: placeholder
-              ? effectiveDataTextStyle.color!.withValues(alpha: 0.6)
+              ? effectiveDataTextStyle.color!.withOpacity( 0.6)
               : null,
         ),
         child: DropdownButtonHideUnderline(child: label),
@@ -593,7 +593,7 @@ class DataTable2 extends DataTable {
     final defaultRowColor = WidgetStateProperty.resolveWith(
       (Set<WidgetState> states) {
         if (states.contains(WidgetState.selected)) {
-          return theme.colorScheme.primary.withValues(alpha: 0.08);
+          return theme.colorScheme.primary.withOpacity(0.08);
         }
         return null;
       },
