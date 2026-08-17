0c8fc59b696507ffd859c59adf03449974a23b22
maxim-saplin <smaxmail@gmail.com>
2026-08-17 08:15:34 +0000

    Deploying to gh-pages from @ maxim-saplin/data_table_2@970d4d11055927a7721d67251d72648883ab8587 🚀

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2191950885" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3528593755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
